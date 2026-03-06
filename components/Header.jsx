5a3ccc2fcf0913973b5b042239db17c8bb683e17
lucariggi92 <riggiluca92@gmail.com>
2026-03-06 11:49:44 +0100

    feat: router app

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header (){
+    return(
+        <>
+        <div className="header">
+
+
+        Visit San Cataldo
+                </div>
+        </>
+    )
+
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layout/DefaultLayout.jsx";
+import Home from "./pages/Home.jsx";
 
 function App() {
- 
+
 
   return (
-    <>
-     ciao
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<Home/>}/> 
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
