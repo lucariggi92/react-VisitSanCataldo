1799e49b04e16922fdf15269fa2119d2c1a8cba3
lucariggi92 <riggiluca92@gmail.com>
2026-03-09 17:57:14 +0100

    feat: hero banner v0

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,8 +3,8 @@ export default function HeroBanner() {
     return (
         <>
             <div className="herobanner pt-5">
-                <div className="container pt-5">
-               <h2 >TRADIZIONE</h2>
+                <div className="container text-heroBanner pt-5">
+               <h2 style={{color:"gray"}} >TRADIZIONE</h2>
                <h2>NATURA</h2>
                <h2>ARTE</h2>
                <h2>CIBO</h2>
