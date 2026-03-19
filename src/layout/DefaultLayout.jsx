8831c45263da976e8aca713512e10abbcddaa6ca
lucariggi92 <riggiluca92@gmail.com>
2026-03-19 13:19:32 +0100

    fix: risolto salto del layout e dell'header durante il cambio rotte

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -4,14 +4,13 @@ import Footer from "../components/Footer";
 
 export default function DefaultLayout() {
     return (
-
-        <>
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
             <Header />
-            <Outlet />
+            {/* Il min-height tiene il footer lontano dall'header anche se i dati non ci sono ancora */}
+            <main style={{ flexGrow: 1, minHeight: '600px' }}> 
+                <Outlet />
+            </main>
             <Footer />
-        </>
-
-
-
+        </div>
     )
 }
