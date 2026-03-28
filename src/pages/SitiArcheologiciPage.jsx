7bd3c17e3da9e6f3ab053bc290fc6c7d9e705a13
lucariggi92 <riggiluca92@gmail.com>
2026-03-28 15:22:12 +0100

    feat

---
+++ src/pages/SitiArcheologiciPage.jsx
@@ -0,0 +1,11 @@
+import ContentGrid from "../components/ContentGrid"
+
+export default function SitiArcheologiciPage(){
+    return(
+        <>
+          <div>
+           <ContentGrid category="siti-archeologici" />
+               </div>
+        </>
+    )
+}
